6794195eba0423a7ffdc231ad1ef08ea6a971e31
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-05-09 23:21:36 -0400

    form completed with styling

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {useSelector} from 'react-redux';
 import Post from './Post/Post.js'
 import useStyles from './styles';
 function Posts() {
     const classes = useStyles();
+    const posts = useSelector((state)=>state.posts);
+    console.log(posts);
     return (
         <div>
             <h1>POSTS</h1>

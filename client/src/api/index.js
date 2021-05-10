0512a1b44a5f6e96847917efb6bdbd2696280b26
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-05-09 23:36:25 -0400

    client now interacts with server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,5 +3,6 @@ import axios from 'axios'
 const url = 'http://localhost:5000/posts'
 
 export const fetchPosts=()=> axios.get(url)
+export const createPost=(newPost)=>axios.post(url, newPost);
 
 //this may need to be changed soon

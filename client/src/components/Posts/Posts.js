a64de1e7d6850b1a4e3952be00b5a768bce0eb9f
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-05-10 01:38:56 -0400

    added cards and buttons on posts, functionality to be added soon

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Grid, CircularProgress} from '@material-ui/core';
 import {useSelector} from 'react-redux';
 import Post from './Post/Post.js'
 import useStyles from './styles';
@@ -7,11 +8,25 @@ function Posts() {
     const posts = useSelector((state)=>state.posts);
     console.log(posts);
     return (
-        <div>
-            <h1>POSTS</h1>
-            <Post/>
-            <Post/>
-        </div>
+        !posts.length? <CircularProgress/> : (
+            <Grid 
+                className={classes.container}
+                container
+                alignItems='stretch'
+                spacing={3}
+            >
+                {posts.map((post)=>(
+                    <Grid 
+                        key={post.id}
+                        item
+                        xs={12}
+                        sm={6}
+                    >
+                        <Post post={post}/>
+                      </Grid>  
+                ))}
+            </Grid>
+        )
     )
 }
 
